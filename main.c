32927120fc6627eaaef8650b4ecd657243a65c6e
Hibajk <hibachenni22@gmail.com>
2023-11-17 16:24:36 -0800

    a

--- main.c
+++ main.c
@@ -12,44 +12,42 @@
 
 int main(int ac, char **argv)
 {
-    int status = 0, nmbr = 0;
-    char *line = NULL;
-    char **command = NULL;
-    
-    (void)ac;
+int status = 0, nmbr = 0;
+char *line = NULL;
+char **command = NULL;
+(void)ac;
 
-    while (1)
-    {
-        line = pro_shell();
-        if (line == NULL)
-        {
-            if (isatty(STDIN_FILENO))
-                write(STDOUT_FILENO, "\n", 1);
-            free(line);
-            return status;
-        }
+while (1)
+{
+line = pro_shell();
+if (line == NULL)
+{
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "\n", 1);
+free(line);
+return (status);
+}
+nmbr++;
+command = cmd_tok(line);
+if (!command)
+{
+free(line);
+continue;
+}
 
-        nmbr++;
-        command = cmd_tok(line);
-        if (!command)
-        {
-            free(line);
-            continue;
-        }
+if (_strcmp(command[0], "exit") == 0)
+{
+free(line);
+freearr(command);
+exit(status);
+}
 
-        if (_strcmp(command[0], "exit") == 0)
-        {
-            free(line);
-            freearr(command);
-            exit(status);
-        }
+if (_strcmp(command[0], "env") == 0)
+printenv(command, &status);
+else
+status = _execution(command, argv, nmbr);
 
-        if (_strcmp(command[0], "env") == 0)
-            printenv(command, &status);
-        else
-            status = _execution(command, argv, nmbr);
-
-        free(line);
-        freearr(command);
-    }
+free(line);
+freearr(command);
+}
 }
