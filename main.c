e8e4055cd08a5446b7b12242947f0e7db9a881ef
Hibajk <hibachenni22@gmail.com>
2023-11-17 17:41:55 -0800

    a

--- main.c
+++ main.c
@@ -5,49 +5,39 @@
 
 /**
  * main - the main functions
- * @ac: arg count
+ * @acc: arg count
  * @argv: arguments
  * Return: 0
  */
 
-int main(int ac, char **argv)
+int main(int acc, char **argv)
 {
-int status = 0, nmbr = 0;
-char *line = NULL;
-char **command = NULL;
-(void)ac;
+	int sts = 0, number = 0;
+	char *inputString = NULL;
+	char **cmd = NULL;
+	(void) acc;
 
-while (1)
-{
-line = pro_shell();
-if (line == NULL)
-{
-if (isatty(STDIN_FILENO))
-write(STDOUT_FILENO, "\n", 1);
-free(line);
-return (status);
-}
-nmbr++;
-command = cmd_tok(line);
-if (!command)
-{
-free(line);
-continue;
-}
+	while (1)
+	{
+		inputString = pro_shell();
+		if (inputString == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			free(inputString), inputString = NULL;
+			return (sts);
+		}
+		number++;
 
-if (_strcmp(command[0], "exit") == 0)
-{
-free(line);
-freearr(command);
-exit(status);
-}
+		cmd = cmd_tok(inputString);
+		if (!cmd)
+			continue;
 
-if (_strcmp(command[0], "env") == 0)
-printenv(command, &status);
-else
-status = _execution(command, argv, nmbr);
-
-free(line);
-freearr(command);
-}
+		if (_strcmp(cmd[0], "exit") == 0)
+			exit(sts);
+		if (_strcmp(cmd[0], "env") == 0)
+			printenv(cmd, &sts);
+		else
+			sts = _execution(cmd, argv, number);
+	}
 }
